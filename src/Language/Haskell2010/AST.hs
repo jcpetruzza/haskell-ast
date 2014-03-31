486f96f43bef2283329aefe1119aecf87fa1ca42
daniel <jcpetruzza@gmail.com>
2014-03-31 11:48:47 +0200

    Can parse Haskell 2010 code

--- src/Language/Haskell2010/AST.hs
+++ src/Language/Haskell2010/AST.hs
@@ -2,7 +2,10 @@
 module Language.Haskell2010.AST
 
 where
+
 import Prelude hiding (exp)
+
+import Control.Applicative
 import Data.Data
 import Data.Foldable (Foldable)
 import Data.Traversable (Traversable)
@@ -14,6 +17,9 @@ import qualified Language.Haskell.AST.Sugar as Sugar
 -- import qualified Language.Haskell.AST.Exts.PatternGuards as PatternGuards
 import qualified Language.Haskell.AST.Exts.FFI as FFI
 
+import qualified Language.Haskell.Exts.Annotated.Syntax as E
+import Language.Haskell.AST.HSE
+
 type Literal = Core.Literal
 
 -- | A Haskell 2010 pattern
@@ -23,10 +29,11 @@ data PatExts id l
     = PatSugar (Sugar.Pat Literal Pat id l)
   deriving (Eq,Ord,Show,Typeable,Data,Foldable,Traversable,Functor)
 
-
 instance Annotated (PatExts id) where
     ann (PatSugar pat) = ann pat
 
+instance HsePat PatExts where
+    fromHsePat p = PatSugar <$> fromHsePat p
 
 -- | A Haskell 2010 expression
 type Exp = Core.Exp LetBinds Pat Literal ExpExts
@@ -38,6 +45,10 @@ newtype ExpExts id l
 instance Annotated (ExpExts id) where
     ann (ExpSugar exp) = ann exp
 
+instance HseExp ExpExts where
+    fromHseExp e = (ExpSugar <$> fromHseExp e)
+               <|> (fromHseFailed e)
+
 
 -- | Haskell 2010 uses pattern guards (we define it here instead
 -- of reusing the one in the PatternGuards module to avoid
@@ -48,27 +59,30 @@ data Guard id l = PatternGuard l [Stmt id l]
 instance Annotated (Guard id) where
     ann (PatternGuard l _) = l
 
+instance HseGuard Guard where
+    fromHseGuard l ss = PatternGuard l <$> mapM fromHseStmt ss
+
 -- | A Haskell 2010 statement
 type Stmt = Sugar.Stmt Binds Exp Pat StmtExts
 
 type StmtExts = NoExts
 
--- | A Haskell 2010 assertion is of the form "C a", with a variable
---   (should be, e.g., a type without context, for FlexibleContexts...)
-type Assertion = Core.Asst Name AssertionExts
-type AssertionExts = NoExts
-
 -- | A Haskell 2010 type
 type Type = Core.Type TypeExts
 
 data TypeExts id l
-  = QualType  (Core.QualType Assertion Type id l)
+  = QualType  (Core.QualType Asst Type id l)
   | TypeSugar (Sugar.Type Type id l)
   deriving (Eq,Ord,Show,Typeable,Data,Foldable,Traversable,Functor)
 
 instance Annotated (TypeExts id) where
     ann (QualType  qty) = ann qty
     ann (TypeSugar t) = ann t
+
+instance HseType TypeExts where
+    fromHseType t = (QualType  <$> fromHseType t)
+                <|> (TypeSugar <$> fromHseType t)
+                <|> (fromHseFailed t)
 
 
 -- | Haskell 2010 binds
@@ -85,8 +99,13 @@ data LetBinds id l
 instance Annotated (LetBinds id) where
     ann (LetBinds l _) = l
 
--- | Haskell 2010 assertions
-type Asst = Core.Asst Name NoExts
+instance HseBinds LetBinds where
+    fromHseBinds (E.BDecls l decls) = LetBinds l <$> mapM fromHseDecl decls
+    fromHseBinds binds = fromHseFailed binds
+
+-- | A Haskell 2010 assertion is of the form "C a", with a variable
+--   (should be, e.g., a type without context, for FlexibleContexts...)
+type Asst = Core.Asst Type NoExts
 
 -- | Haskell 2010 class and instance declarations
 type ClassRelatedDecl = Core.ClassRelatedDecl Asst Type Bind ClassBodyExts InstBodyExts ClassRelExts
